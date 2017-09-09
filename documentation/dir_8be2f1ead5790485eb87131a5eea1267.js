f7ed419cc831eb20dae0ce9212becbd37e51d2f7
Travis CI <travis@example.org>
2017-09-09 19:07:34 +0000

    Updated documentation from 86d3fcdb78a4ceca76cbe3397955eca303d134ef

---
+++ documentation/dir_8be2f1ead5790485eb87131a5eea1267.js
@@ -0,0 +1,6 @@
+var dir_8be2f1ead5790485eb87131a5eea1267 =
+[
+    [ "importer.cpp", "importer_8cpp.html", [
+      [ "OsmXmlParser", "class_importer_1_1_osm_xml_parser.html", "class_importer_1_1_osm_xml_parser" ]
+    ] ]
+];
---
+++ documentation/navtreedata.js
@@ -0,0 +1,60 @@
+var NAVTREE =
+[
+  [ "alaCarte Maps", "index.html", [
+    [ "The Exception Guide", "md_doc_related-pages_exception-guide.html", null ],
+    [ "The Git Guide", "md_doc_related-pages_git-guide.html", null ],
+    [ "The Macro Guide", "md_doc_related-pages_macro-guide.html", null ],
+    [ "The Test Guide", "md_doc_related-pages_test-guide.html", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"alacarte__importer_8cpp.html",
+"class_color.html#af64aa4218f16e84f21f2f6241404efd0",
+"class_maybe_cached_string.html#adf7dda4efa7bf7523bf390cc273d86dd",
+"class_request_manager_1_1_running_queue.html#aa4a37fd6bb0420bf24f23add57f06883",
+"class_tag_matches_selector.html#af11b35d974058988307514235badc2f5",
+"configuration_8hpp.html#ab7eea6a786eaae2c64c6dbfdddce9e56",
+"http__server_8hpp.html",
+"selectors_8hpp_source.html",
+"stylesheet__manager_8hpp.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
